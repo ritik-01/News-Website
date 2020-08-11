40fd3e50b1413806e8462af467b4100c04be7bd1
ritik <ritik-01>
2020-08-11 12:08:21 +0530

    image added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.warn('Project 3');
 
 let newsBox = document.getElementById('accordion');
 
-api = 'Your Key';
+api = '8735692b77534305b67e00adfabaec44';
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', `https://newsapi.org/v2/top-headlines?country=in&apiKey=${api}`, true);
